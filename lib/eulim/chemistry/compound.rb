429f53f8e3927e30a9de10423b477f6e08284eeb
syedfazilbasheer-quester <fazil.basheer@quester.xyz>
2017-05-24 06:39:38 +0530

    Compound#get_constituent_atoms fixed, stored on initiazed

--- lib/eulim/chemistry/compound.rb
+++ lib/eulim/chemistry/compound.rb
@@ -2,16 +2,15 @@ module Eulim::Chemistry
 	class Compound
 
 		VALID_COMPOUND_REGEXP = /[A-Z][a-z]{0,2}\d*|\((?:[^()]*(?:\(.*\))?[^()]*)+\)\d*/
-		VAILD_ELEMENT_REGEXP = /[A-Z][a-z]{0,2}\d*/
 
 		def initialize(arg)
 			@formula = arg
+			@constituent_atoms = get_constituent_atoms
 		end
 
 		def molecular_mass
 			mass = 0
-			element_atom_count = get_element_atom_count
-			element_atom_count.each do |symbol, count|
+			@constituent_atoms.each do |symbol, count|
 				mass += Element.get_by_symbol(symbol).atomic_mass * count
 			end
 			mass
@@ -19,46 +18,32 @@ module Eulim::Chemistry
 
 		def elements
 			elements = []
-			element_atom_count = get_element_atom_count
-			element_atom_count.each do |symbol, count|
-				elements << Element.get_by_symbol(symbol)
+			@constituent_atoms.each do |symbol, count|
+				elements << {element: Element.get_by_symbol(symbol), atom_count: count}
 			end
 			elements
 		end
 
 		private
-			def get_element_atom_count formula = @formula, group_multiplier = 1, result = {}, groups = []
-				formula
-				elements_n_groups = formula.scan VALID_COMPOUND_REGEXP
-				loop do
-					break if !elements_n_groups.last
-					elements = elements_n_groups.last.scan VAILD_ELEMENT_REGEXP
-					if elements.first === elements_n_groups.last
-						element_multipler = elements.first.match(/\d*$/).to_a.first.to_i
-						element_multipler = element_multipler > 0 ? element_multipler : 1
-						element_symbol = elements.first.match(/[A-Z][a-z]{0,2}/).to_a.first
-						if result[element_symbol]
-							result[element_symbol] += (element_multipler * group_multiplier)
-						else
-							result[element_symbol] = (element_multipler * group_multiplier)
-						end
+			def get_constituent_atoms formula=@formula, result={}
+				constituents = formula.scan VALID_COMPOUND_REGEXP
+				constituents.each do |constituent|
+					multipler = get_multipler constituent
+					if constituent[0] != '(' && multipler == 0
+						result[constituent] = result[constituent] ? result[constituent] + 1 : 1
 					else
-						groups << elements_n_groups.last
-					end
-					elements_n_groups.pop
-				end
-				loop do
-					break if !groups.last
-					group = groups.pop
-					grml = get_group_multiplier(group)
-					get_element_atom_count group[1..-(grml.to_s.length+1)], grml*group_multiplier, result, groups
+						(multipler == 0 ? 1 : multipler).times do
+							sub_constituents = constituent.match(/^\(?(.*?)\)?($|\d*$)/).to_a
+							idx = constituent == sub_constituents.first ? 1 : 0
+							get_constituent_atoms sub_constituents[idx], result
+						end
+					end	
 				end
 				result
 			end
 
-			def get_group_multiplier formula
-				group_multiplier = formula.scan(VALID_COMPOUND_REGEXP).first.match(/\d*$/).to_a.first.to_i
-				group_multiplier > 0 ? group_multiplier : 1
+			def get_multipler constituent
+				multipler = constituent.match(/\d*$/).to_a.first.to_i
 			end
 
 	end
