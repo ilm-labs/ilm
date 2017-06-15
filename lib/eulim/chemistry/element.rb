da293a411cbc5a5fe16013d87139866c4e0d6104
Rohith <rohith.iitb@gmail.com>
2017-06-15 19:11:03 +0530

    final working completely for elements only need to work on efficiently wring the array loop may be hashes for each attribute

--- lib/eulim/chemistry/element.rb
+++ lib/eulim/chemistry/element.rb
@@ -13,10 +13,10 @@ module Eulim
       @@array = Array.new
       
       def initialize(arg)
-        @name = arg[1]
-        @symbol = arg[0]
-        @atomic_number = arg[2]
-        @atomic_mass = arg[3]
+        @name = arg[1].to_s.strip
+        @symbol = arg[0].to_s.strip
+        @atomic_number = arg[2].to_s.strip
+        @atomic_mass = arg[3].to_s.strip
       end
       
       CSV.foreach(File.join(File.dirname(__FILE__), "elements.csv"), headers: true) do |row|
@@ -24,24 +24,22 @@ module Eulim
       end
 
       private_class_method def self.method_missing(m, *args)
-
         attribute = m.to_s.split('get_by_').last
         valid_method? m, attribute
+        raise  'please give argument' unless !args.empty?
         args[0] = attribute == 'name' ? args[0].capitalize : args[0]
         element_data = get_element_by_attribute attribute, args[0]
         raise 'Element not found' unless element_data
         element_data
       end
       
-      def attrs
-        instance_variables.map{|ivar| instance_variable_get ivar}
-      end
-
       private_class_method def self.get_element_by_attribute(attribute, value)
         @@array.each do |element|
-          element.send(attribute.to_sym) == value
-          return element
+          if element.instance_variable_get(("@"+attribute).intern) == value  ## elements value of attribute == value give by user
+            return element
+          end
         end
+        raise(NameError, 'element not found')
       end
 
       private_class_method def self.valid_method?(m, attribute)
