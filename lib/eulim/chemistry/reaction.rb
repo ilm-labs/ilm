e2f5fb7abc5c15c7fa98ab9aa245f6f99e4428d2
syedfazilbasheer-quester <fazil.basheer@quester.xyz>
2017-07-08 16:13:52 +0530

    few rubocop compliances

--- lib/eulim/chemistry/reaction.rb
+++ lib/eulim/chemistry/reaction.rb
@@ -18,8 +18,8 @@ module Eulim
         @equation = args[:equation]
         @species = build_species
         @is_valid = valid_rxn?
-        participant_elements
-        @balanced_eqn = balance_rxn
+        p participant_elements
+        # @balanced_eqn = balance_rxn
         @rate_equation = validify_rate_eqn args[:rate_equation]
       end
 
@@ -107,13 +107,14 @@ module Eulim
         @participants = participants.flatten.uniq
       end
 
-      def get_participant_row(participant)
+      def get_participant_row(parti)
         row = []
         @species.keys.each do |key|
           i = key == :reactants ? 1 : -1
           @species[key].keys.each do |specie|
-            if specie.include? participant
-              row << @species[key][specie][:compound].constituents[participant][:atom_count] * i
+            if specie.include? parti
+              row << @species[key][specie][:compound]
+                     .constituents[parti][:atom_count] * i
             else
               row << 0
             end
@@ -124,8 +125,8 @@ module Eulim
 
       def write_matrix
         @matrix = Matrix[]
-        @participants.each do |participant|
-          @matrix = Matrix.rows(@matrix.to_a << get_participant_row(participant))
+        @participants.each do |parti|
+          @matrix = Matrix.rows(@matrix.to_a << get_participant_row(parti))
         end
         @matrix
       end
